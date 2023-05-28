bc58565bdbb87c05c2c3b968d4e8188ade2148af
mihadereani <110335459+mihadereani@users.noreply.github.com>
2023-05-28 13:57:16 +0200

    task 2.5

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import welcome
+from .views import RecipeListView
+from .views import RecipeDetailView
 
 app_name = 'recipes'
 
 urlpatterns = [
     path('', welcome, name='home'),
+    path('list/', RecipeListView.as_view(), name='list'),
+    path('list/<pk>', RecipeDetailView.as_view(), name='detail'),
 ]
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+# to display lists and details
+from django.views.generic import ListView, DetailView
+from .models import Recipe  # to access Recipe model
 
 # Create your views here.
 
 
 def welcome(request):
     return render(request, 'recipes/recipes_home.html')
+
+
+class RecipeListView(ListView):  # class-based 'protected' view
+    model = Recipe  # specify model
+    template_name = 'recipes/main.html'  # specify template
+
+
+class RecipeDetailView(DetailView):  # class-based 'protected' view
+    model = Recipe  # specify model
+    template_name = 'recipes/detail.html'  # specify template
