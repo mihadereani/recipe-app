b8f58c744d3db9cce66d26689323073d26235269
mihadereani <110335459+mihadereani@users.noreply.github.com>
2023-05-28 14:04:51 +0200

    add tests

--- src/recipes/tests.py
+++ src/recipes/tests.py
@@ -23,4 +23,14 @@ class RecipeModelTest(TestCase):
     def test_cookingtime_helptext(self):
         recipe = Recipe.objects.get(id=1)
         recipe_cookingtime = recipe._meta.get_field('cooking_time').help_text
-        self.assertEqual(recipe_cookingtime, 'in minutes')
+        self.assertEqual(recipe_cookingtime, 'In minutes')
+
+    def test_get_absolute_url(self):
+        recipe = Recipe.objects.get(id=1)
+        # get_absolute_url() should load the URL /list/1                # TEST TO CHECK get_absolute_url() FUNCTIONALITY
+        self.assertEqual(recipe.get_absolute_url(), '/list/1')
+
+    def test_difficulty_calculation(self):
+        # TEST TO CHECK calculate_difficulty() FUNCTIONALITY
+        recipe = Recipe.objects.get(id=1)
+        self.assertEqual(recipe.calculate_difficulty(), 'Easy')
