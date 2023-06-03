188a66f7d967cd5591fe77111904e4f8a2608fbe
mihadereani <110335459+mihadereani@users.noreply.github.com>
2023-06-03 20:06:52 +0200

    implement search bar

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import welcome
 from .views import RecipeListView
 from .views import RecipeDetailView
+from .views import records
 
 app_name = 'recipes'
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('', welcome, name='home'),
     path('list/', RecipeListView.as_view(), name='list'),
     path('list/<pk>', RecipeDetailView.as_view(), name='detail'),
+    path('search/', records, name='search'),
 ]
